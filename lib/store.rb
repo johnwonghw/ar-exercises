7c69b91d836146ae2656417aac6a6f7c74c2b91e
John <john.wong.h@gmail.com>
2017-08-02 20:25:39 +0000

    Added function code to complete exercise 7 by adding validations and testcode to create new store.

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,15 @@
 class Store < ActiveRecord::Base
 
- # has_many 
+  has_many :employees
+
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
+  validate :mens_or_womens_apparel_true
+
+  def mens_or_womens_apparel_true
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel_womens_apparel, "mens_apparel or womens_apparel must be true")
+    end
+  end
 
 end
